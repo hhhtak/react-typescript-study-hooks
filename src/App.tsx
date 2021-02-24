78c8f1977ce6d1690cd6aa56ba4c3533f8758bd8
h_takamatsu <h_takamatsu@kost.jp>
2021-02-24 09:54:05 +0900

    create Row component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { requests } from './request'
+import { Row } from './components/Row'
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="TOP Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchTopRated} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row
+        title="Documentaries Movies"
+        fetchUrl={requests.fetchDocumentMovies}
+      />
+    </div>
+  )
 }
 
 export default App
--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
 const instance: any = axios.create({
-  baseUrl: 'https://api.themoviedb.org/3',
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest',
+  },
+  responseType: 'json',
 })
 
 export default instance
