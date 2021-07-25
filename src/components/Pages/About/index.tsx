f8c2e99844af137fb910d66b1fb80b4a6182f79b
hhhtak <matuyamen@hotmail.co.jp>
2021-07-25 15:34:25 +0900

    set Observe

--- src/components/Pages/About/index.tsx
+++ src/components/Pages/About/index.tsx
@@ -2,7 +2,50 @@ import * as React from 'react'
 import './About.css'
 
 const About: React.FC = () => {
-  return <div className="about"></div>
+  const fixedHeaderObserve = () => {
+    const headerElement = document.querySelector('#target-header')
+    const targetElement = document.querySelector('.js-header-observer')
+
+    if (!headerElement || !targetElement) return
+
+    const observeHandler = (entries: any) => {
+      headerElement.setAttribute(
+        'data-active',
+        String(!entries[0].isIntersecting),
+      )
+    }
+
+    const headerObserver = new window.IntersectionObserver(observeHandler)
+    headerObserver.observe(targetElement)
+  }
+
+  document.addEventListener('DOMContentLoaded', fixedHeaderObserve, false)
+
+  return (
+    <div>
+      <header id="target-header" className="global-header" data-active="false">
+        <div>
+          <h1 className="logo">Intersection Observer API</h1>
+        </div>
+      </header>
+      <main className="main">
+        <div className="contents">
+          <div className="contents-item js-header-observer"></div>
+          <div className="contents-item"></div>
+          <div className="contents-item"></div>
+          <div className="contents-item"></div>
+          <div className="contents-item"></div>
+          <div className="contents-item"></div>
+          <div className="contents-item"></div>
+          <div className="contents-item"></div>
+          <div className="contents-item"></div>
+          <div className="contents-item"></div>
+          <div className="contents-item"></div>
+          <div className="contents-item"></div>
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export default About
